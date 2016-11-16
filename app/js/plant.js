8e63c7545ceb7a3f5ee2852005d18ecbb7842545
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-16 15:23:46 -0600

    plant has a property of height

--- app/js/plant.js
+++ app/js/plant.js
@@ -1,7 +1,8 @@
 // PlantFunction is the prototype of tree
 let PlantFunction = {
   testFunction() {return `My number is ${this.a}`},
-  a: 1
+  a: 1,
+  height: null
 };
 
 module.exports = PlantFunction;
